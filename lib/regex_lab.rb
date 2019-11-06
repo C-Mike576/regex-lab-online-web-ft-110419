eff4b399450bc91fd872c389179bd3d4f03f1157
Michael Calkins <m.calkins576@gmail.com>
2019-11-06 12:42:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- p text.scan(/\b^[un][ing]$\b/)
+ p text.scan(/^un\w$ing/)
 end
 
 def words_five_letters_long(text)
